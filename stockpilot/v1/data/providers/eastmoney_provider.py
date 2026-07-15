4d1e552a0b390ee9699fa2d0c58cd3d859faa5c0
huaiyaho <58797510+huaiyaho@users.noreply.github.com>
2026-07-15 13:59:45 +0800

    Add eastmoney provider adapter skeleton

---
+++ stockpilot/v1/data/providers/eastmoney_provider.py
@@ -0,0 +1,12 @@
+"""
+EastMoney Provider adapter placeholder.
+Reserved as alternative market data source.
+"""
+
+
+class EastMoneyProvider:
+    def get_stocks(self):
+        return []
+
+    def get_daily_prices(self, code, start=None, end=None):
+        return []
